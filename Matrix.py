70278d8a41bc22364b688990c26268f43dedf397
MeryChon <mchon14@freeuni.edu.ge>
2017-12-27 20:39:10 +0400

    Bugs fixed. Standartization complete

--- Matrix.py
+++ Matrix.py
@@ -7,7 +7,12 @@ class Matrix:
         self.matrix = self.create_matrix(data)
         self.column_order = [i for i in range(1, self.num_columns+1)]
         self.gauss_jordan()
-        print(self.column_order)
+
+    def get_matrix(self):
+        return self.matrix
+
+    def get_column_order(self):
+        return self.column_order
 
     @staticmethod
     def create_matrix(data):
@@ -23,40 +28,26 @@ class Matrix:
         for i in range(self.num_columns):
             self.matrix[from_index][i] = self.matrix[from_index][i] ^ to_subtr[i]
 
-    def subtract_from_lower_rows(self, row_num, pivot_index):
-        pivot = self.matrix[row_num][pivot_index]
-        # print(row_num, pivot_index, self.num_rows, len(self.matrix))
-        for i in range(row_num+1, self.num_rows):
-            if self.matrix[i][pivot_index] != 0:
+    def subtract_from_other_rows(self, row_num, pivot_index):
+        for i in range(self.num_rows):
+            if i != row_num and self.matrix[i][pivot_index] != 0:
                 self.subtract_rows(i, row_num)
-        self.print_matrix(self.matrix)
 
     def swap_columns(self, first, second):
         for i in range(self.num_rows):
-            # tmp = self.matrix[first][i]
-            # self.matrix[first][i] = self.matrix[second][i]
-            # self.matrix[second][i] = tmp
             self.matrix[i][first], self.matrix[i][second] = self.matrix[i][second], self.matrix[i][first]
-        # tmp = self.column_order[first]
-        # self.column_order[first] = second
-        # self.column_order[second] = tmp
         self.column_order[first], self.column_order[second] = self.column_order[second], self.column_order[first]
 
     def gauss_jordan(self):
         pivot_index = -1
-        self.print_matrix(self.matrix)
         for i in range(self.num_rows):
             pivot_index += 1
             if self.matrix[i][i] == 0:
                 for j in range(self.num_columns):
                     if self.matrix[i][j] != 0:
                         self.swap_columns(i, j)
-                        print("========== Exchange of columns ========")
-                        print(self.column_order)
-                        print()
-                        self.print_matrix(self.matrix)
                         break
-            self.subtract_from_lower_rows(i, pivot_index)
+            self.subtract_from_other_rows(i, pivot_index)
 
     @staticmethod
     def print_matrix(matrix):
--- StandardForm.py
+++ StandardForm.py
@@ -6,9 +6,23 @@ def read_from_file(file_name):
     info = file_obj.read().split()
     return info
 
+def write_to_file(file_name, matrix, column_order):
+    file_obj = open(file_name, "w")
+    to_write = str(len(matrix[0])) + " " + str(len(matrix)) + "\n"
+    for r in matrix:
+        for i in r:
+            to_write += str(i)
+        to_write+="\n"
+    for i in column_order:
+        to_write += str(i) + " "
+    to_write.strip()
+    file_obj.write(to_write)
+
+
 
 if __name__ == '__main__':
     infile_name = sys.argv[1]
     outfile_name = sys.argv[2]
     data = read_from_file(infile_name)
     matrix = Matrix(data[1], data[0], data[2:])
+    write_to_file(outfile_name, matrix.get_matrix(), matrix.get_column_order())
